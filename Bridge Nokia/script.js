3248b3c6da035f1a508269387f5c69c6eb69b9d7
Esteban <estebangabriel990@gmail.com>
2025-09-05 14:56:34 +0000

    Alteração

--- Bridge Nokia/script.js
+++ Bridge Nokia/script.js
@@ -4,22 +4,23 @@ function gerarComandos() {
     const portaPon = Document.getElementById('inputGpon').value;
     const posicaoONT = Document.getElementById('inputIndex').value;
     const portaLAN = Document.getElementById('portaLan').value;
-}
-
-/* 
-function gerarComandos() {
-    // Obter valores dos campos de entrada
-    const slot = document.getElementById('inputSlot').value;
-    const portaPon = document.getElementById('inputGpon').value;
-    const posicaoOnt = document.getElementById('inputIndex').value;
-    const portaLan = document.getElementById('portaLan').value;
-    const cardType = document.getElementById('CardType').value;
+    const cardType = Document.getElementById('CardType').value;
     const desc1 = document.getElementById('inputDesc1').value;
     const desc2 = document.getElementById('inputDesc2').value;
     const sernum = document.getElementById('inputSernum').value;
     const vlan = document.getElementById('ProvVLAN').value;
     const portaVoip = document.getElementById('portaVoip').value;
-    
-    // Continua no próximo passo...
+
+    if(!slot || !portaPon || !posicaoONT || !sernum) {
+        alert('Por favor, preencha todos os campos obrigatórios: Slot, Porta PON, Posição da ONT e ALCL da ONT');
+        return;      
 }
-/*
+
+
+
+
+
+
+
+
+}
