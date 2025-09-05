d90a697a92ad37a1cfa6a2a3756e3d74e7468ca2
Esteban <estebangabriel990@gmail.com>
2025-09-05 01:03:44 +0000

    Evolução

---
+++ Bridge Nokia/script.js
@@ -0,0 +1,25 @@
+function gerarComandos() {
+    // Obter valores dos campos de entrada
+    const slot = document.getElementById('inputSlot').value;
+    const portaPon = Document.getElementById('inputGpon').value;
+    const posicaoONT = Document.getElementById('inputIndex').value;
+    const portaLAN = Document.getElementById('portaLan').value;
+}
+
+/* 
+function gerarComandos() {
+    // Obter valores dos campos de entrada
+    const slot = document.getElementById('inputSlot').value;
+    const portaPon = document.getElementById('inputGpon').value;
+    const posicaoOnt = document.getElementById('inputIndex').value;
+    const portaLan = document.getElementById('portaLan').value;
+    const cardType = document.getElementById('CardType').value;
+    const desc1 = document.getElementById('inputDesc1').value;
+    const desc2 = document.getElementById('inputDesc2').value;
+    const sernum = document.getElementById('inputSernum').value;
+    const vlan = document.getElementById('ProvVLAN').value;
+    const portaVoip = document.getElementById('portaVoip').value;
+    
+    // Continua no próximo passo...
+}
+/*
