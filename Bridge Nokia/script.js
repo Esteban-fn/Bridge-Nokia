49052e2ee2280d3a9ebc678a4a0ae071a811f757
Esteban <estebangabriel990@gmail.com>
2025-09-12 13:56:41 +0000

    Funcional

--- Bridge Nokia/script.js
+++ Bridge Nokia/script.js
@@ -105,7 +105,7 @@ configure equipment ont interface 1/1/${slot}/${pon}/${pos} admin-state down
 configure equipment ont no interface 1/1/${slot}/${pon}/${pos}
 
 2° PASSO -> PROVISIONAR ONT:
-configure equipment ont interface 1/1/${slot}/${pon}/${pos} sw-ver-pland auto desc1 "${desc1}" desc2 "${desc2}" sernum ${sernum} subslocid WILDCARD fec-up disable optics-hist enable sw-dnload-version disabled voip-allowed veip log-auth-pwd plain:** pland-cfgfile1 auto dnload-cfgfile1 auto planned-us-rate nominal-line-rate
+configure equipment ont interface 1/1/${slot}/${pon}/${pos} sw-ver-pland auto desc1 ${desc1} desc2 ${desc2} sernum ${sernum} subslocid WILDCARD fec-up disable optics-hist enable sw-dnload-version disabled voip-allowed veip log-auth-pwd plain:** pland-cfgfile1 auto dnload-cfgfile1 auto planned-us-rate nominal-line-rate
 
 configure equipment ont interface 1/1/${slot}/${pon}/${pos} admin-state up
 
@@ -116,8 +116,8 @@ configure qos interface 1/1/${slot}/${pon}/${pos}/${cardType}/${portaLAN} upstre
 
 configure interface port uni:1/1/${slot}/${pon}/${pos}/${cardType}/${portaLAN} admin-up
 configure bridge port 1/1/${slot}/${pon}/${pos}/${cardType}/${portaLAN} max-unicast-mac 12 max-committed-mac 1
-configure bridge port 1/1/${slot}/${pon}/${pos}/${cardType}/${portaLAN} vlan-id "${vlan}"
-configure bridge port 1/1/${slot}/${pon}/${pos}/${cardType}/${portaLAN} pvid "${vlan}"
+configure bridge port 1/1/${slot}/${pon}/${pos}/${cardType}/${portaLAN} vlan-id ${vlan}
+configure bridge port 1/1/${slot}/${pon}/${pos}/${cardType}/${portaLAN} pvid ${vlan}
 
 4° PASSO -> Comando TL1:
 ENT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${slot}-${pon}-${pos}-30::::PARAMNAME=InternetGatewayDevice.X_ASB_COM_EthPort.EthPort.${portaLAN}.isTr069Domain,PARAMVALUE=false;
